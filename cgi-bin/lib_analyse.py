4fb7e86751fa11cd8d18e7f2be418f462f5d6625
Adrien Andre <adr.andre@laposte.net>
2020-06-21 14:36:31 -0400

    Add dependency list

--- cgi-bin/lib_analyse.py
+++ cgi-bin/lib_analyse.py
@@ -21,52 +21,52 @@
 ##                                                                       ##
 ###########################################################################
 
-import os, time, commands, OsmApi, sys, traceback, re
+import os, time, commands, osmapi, sys, traceback, re
 import lib_translate
 
 work  = "/data/work/analyser2/"
 
 class analyser():
-    
+
     def __init__(self, relid, force, lg):
-        
+
         self._check = {}
         self._WaysJoined = []
         self._tr = lib_translate.translator(lg)
         Tr = self._tr.TranslateKey
-        
+
         if "/" in relid or "." in relid:
             sys.exit(0)
-            
+
         if not force and os.path.exists(os.path.join(work, "%s-res-d.html"%relid)):
             sys.exit(0)
-        
+
         try:
             self._relid = int(relid)
             print "Num_OK"
         except:
             self._relid = relid
             self.error(Tr("err.InvalidNumber"))
-                    
+
         try:
             self.GetOsmFile()
             print "GetFile OK"
         except:
             self.error(Tr("err.WhileDL"))
-            
+
         try:
             self.GetData()
             print "GetData OK"
         except:
             self.error(Tr("err.RelationNotExists"))
-            
+
         try:
             self.Check()
             print "Check OK"
         except:
             traceback.print_exc(file=open(work + "error.txt","w"))
             self.error(Tr("err.WhileTesting"))
-            
+
         try:
             self.WriteFiles()
             print "Write OK"
@@ -76,27 +76,27 @@ class analyser():
 
     def GetOsmFile(self):
         s,o = commands.getstatusoutput("wget http://api.openstreetmap.fr/api/0.6/relation/%d/full --no-cache -O "%self._relid + os.path.join(work, "%d.osm"%self._relid))
-    
+
     def error(self, texte):
         #text = texte
         open(os.path.join(work, "%s.osm"%str(self._relid)), "w").write('<?xml version="1.0" encoding="UTF-8"?>\n<osm version="0.6">\n</osm>')
-        open(os.path.join(work, "%s-res-d.html"%str(self._relid)), "w").write("")            
+        open(os.path.join(work, "%s-res-d.html"%str(self._relid)), "w").write("")
         text = u"<font color=\"#CC0000\">%s</font>"%texte
         open(os.path.join(work, "%s-res-g.html"%str(self._relid)), "w").write(text.encode("utf8"))
         sys.exit(0)
-                    
+
     def GetData(self, src = "api"):
 
         #get data
         if src=="api":
-            Api = OsmApi.OsmApi()
+            Api = osmapi.OsmApi()
             try:
                 DataRel = Api.RelationFull(self._relid)
                 if not DataRel:
                     self.error("La relation n'existe pas")
             except:
                 self.error("La relation n'existe pas")
-                
+
         elif src=="bin":
             try:
                 import Pyro.core
@@ -105,24 +105,24 @@ class analyser():
                 if not DataRel:
                     self.error("La relation n'existe pas")
             except:
-                self.error("La relation n'existe pas")        
-                
+                self.error("La relation n'existe pas")
+
         #
         self._relations = [ c[u'data'] for c in DataRel if c[u'type']==u'relation' ]
         self._relation  = {}
         for rel in self._relations:
             self._relation[rel[u'id']]=rel
-        
+
         self._ways = [ c[u'data'] for c in DataRel if c[u'type']==u'way' ]
         self._way  = {}
         for way in self._ways:
             self._way[way[u'id']]=way
-        
+
         self._nodes = [ c[u'data'] for c in DataRel if c[u'type']==u'node' ]
         self._node  = {}
         for node in self._nodes:
             self._node[node[u'id']]=node
-        
+
         #Bbox
         self._mLon = float(180)
         self._mLat = float(90)
@@ -139,7 +139,7 @@ class analyser():
         self._MLat += 0.001
 
     def _JoinWays(self):
-        
+
         self._WaysJoined = []
         WaysATraiter = [ way[u'nd'] for way in self._ways ]
         List = []
@@ -188,16 +188,16 @@ class analyser():
                                     self._WaysJoined.append(List)
                                     Jump = True
 
-                                    
+
 ####################################################
 #                    Checks                        #
 ####################################################
 
     def Check(self):
-        
+
         CheckAll = self._CheckAdministrative
         CheckAll()
-        
+
 #####      Administrative boundaries    #####
 
     def _CheckAdministrative(self):
@@ -205,22 +205,22 @@ class analyser():
             self._JoinWays()
         self._CheckOpen()
         self._CheckIntersect()
-    
+
     def _CheckOpen(self):
-        
+
         ReturnList = []
         Tr = self._tr.TranslateKey
-        
+
         for List in self._WaysJoined:
             if not (List[0][0] == List[-1][-1]):
                 ReturnList.append((self._node[List[0][0]][u'lon'],self._node[List[0][0]][u'lat']))
                 ReturnList.append((self._node[List[-1][-1]][u'lon'],self._node[List[-1][-1]][u'lat']))
-        
+
         self._check[u'open'] = (Tr('test.open'),ReturnList)
-            
+
 
     def _CheckIntersect(self):
-        
+
         self._JoinWays()
         ReturnList = []
         ReAllPoints = re.compile("[\(,][^\(,\)]*[\),]")
@@ -240,9 +240,9 @@ class analyser():
             for point in ReAllPoints.findall(o):
                 lon, lat = point[1:-1].strip().split(" ")
                 ReturnList.append((float(lon),float(lat)))
-            
+
         self._check[u'intersect'] = (Tr('test.intersect'),ReturnList)
-    
+
 ####################################################
 #                   Results                        #
 ####################################################
@@ -251,12 +251,12 @@ class analyser():
         self._WriteGauche()
         self._WriteDroite()
         self._WriteBubbleText()
-    
+
     def _WriteGauche(self):
-        
+
         Tr = self._tr.TranslateKey
         Text=[]
-        
+
         JosmLink = u"http://localhost:8111/import?url=http://api.openstreetmap.fr/api/0.6/relation/%d/full"%self._relid
         Text.append(u"<div id='EditRel' >" + Tr('editrel')%u"<a class='black' href='%s' target='hiddenIframe'>JOSM</a>"%JosmLink)
         Text.append(u"</div>")
@@ -266,34 +266,34 @@ class analyser():
             Text.append(u'&nbsp;<img src="../www/markers/%s-l.png">'%test)
             Text.append(u"<input class='chk' id='%s' type='checkbox' name='%s' onclick=\"Toogle_Chk('%s')\">%s (%d) "%(test,test,test,Res[0],len(Res[1])))
             Text.append(u"<br>")
-       
+
         fileg = open(os.path.join(work, "%d-res-g.html"%self._relid), "w")
         Text = [ l.encode("utf8") for l in Text]
         fileg.write("\n".join(Text))
         fileg.close()
-    
+
     def _WriteDroite(self):
-                
+
         Tags = self._relation[self._relid][u'tag']
         Text = []
         Text.append(u"<table>")
         for tag in Tags:
             Text.append(u"<tr><td class='g'>%s</td><td class='d'>%s</td></tr>"%(tag,Tags[tag]))
         Text.append(u"</table>")
-        
+
         filed = open(os.path.join(work, "%d-res-d.html"%self._relid), "w")
         Text = [ l.encode("utf8") for l in Text]
         filed.writelines(Text)
         filed.close()
-        
+
     def _WriteBubbleText(self):
-        
+
         Tr = self._tr.TranslateKey
         for test in self._check:
-            
+
             ListNodes = self._check[test][1]
             Text = []
-            
+
             Text.append(u"lat\tlon\tmarker_id\ticon\ticonSize\ticonOffset\thtml\n")
             for Node in ListNodes:
                 lon , lat = Node
@@ -303,7 +303,7 @@ class analyser():
                 JosmLink = 'http://localhost:8111/load_and_zoom?left=%f&bottom=%f&right=%f&top=%f'%(lon - e,lat - e,lon + e,lat + e)
                 html = u'<div class="green" >' + Tr('editzone')%u'<a class="green" target="hiddenIframe" href="%s">JOSM</a></div>'%JosmLink
                 Text.append(u"%s\t%s\t%s\t%s\t%s\t%s\t%s\n"%(lat,lon,mid,ic,'17,33','-8,-33',html))
-            
+
             file = open(os.path.join(work, "%s-%s.txt"%(self._relid,test)), "w")
             Text = [ l.encode("utf8") for l in Text]
             file.writelines(Text)
